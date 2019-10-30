26362bc35431987cd33fcf3fbaeb876f775b283d
Joshua Tucker <thejltucker@gmail.com>
2019-10-30 15:06:19 -0400

    Modified file structure to accommodate Areas in the MVC routing

--- Spice/Areas/Customer/Controllers/HomeController.cs
+++ Spice/Areas/Customer/Controllers/HomeController.cs
@@ -7,8 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Spice.Models;
 
-namespace Spice.Controllers
+namespace Spice.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
