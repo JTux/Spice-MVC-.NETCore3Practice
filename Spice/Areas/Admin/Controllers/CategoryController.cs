3b489f9c9b7e93ef7fb558973767cf07cccd8dfb
Joshua Tucker <thejltucker@gmail.com>
2019-10-31 11:09:36 -0400

    Created Category Model/Controller/Index View along with added Images and other styling changes

---
+++ Spice/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET Admin/Category/Index
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _context.Categories.ToListAsync();
+
+            return View(categories);
+        }
+    }
+}
